45a09e376f8bb9606d5c4f8a09036540e0bcfcd4
Vadym Chutur <falloutgreck@gmail.com>
2022-10-22 01:22:21 +0300

    Update search-image.js

--- src/js/search-image.js
+++ src/js/search-image.js
@@ -83,6 +83,16 @@ function createMarkup(hits) {
   if (searchApiServise.isShowLoadMore) {
     io.observe(target);
   }
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+
   return gallery;
 }
 
