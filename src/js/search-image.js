0156fb631d429b4e89436920f806ad02c8dbd334
Vadym Chutur <falloutgreck@gmail.com>
2022-10-22 02:05:36 +0300

    Update search-image.js

--- src/js/search-image.js
+++ src/js/search-image.js
@@ -74,7 +74,7 @@ function onLoad(hits, observer) {
     .firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
-    top: cardHeight * 2.5,
+    top: cardHeight * 2,
     behavior: 'smooth',
   });
 
